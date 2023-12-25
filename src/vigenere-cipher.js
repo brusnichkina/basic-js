3c906de45deedd6c54f6ce5fb0ecd29c2707768c
brusnichkina <bru_snichkina@icloud.com>
2023-12-25 10:55:12 +0300

    feat: add viginere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,84 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  constructor (machine = true) {
+    this.machine = machine;
+  };
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    else {
+      if (key.length < message.length) {
+        for (let i = 0; i < message.length; i++) {
+          key += key[i % key.length];
+        };
+      };
+    key = key.toUpperCase();
+    let mes = message.toUpperCase().match(/[a-z]/gi);
+    if (mes !== null) {
+      mes = mes.join('');
+    };
+    let result = '';
+    let num = 0;
+    for (let i = 0; i < message.length; i++) {     
+      if (message[i].match(/[a-z]/gi) !== null ) {
+        let a = mes.charCodeAt(num) - 65;
+        let b = key.charCodeAt(num) - 65;
+        result += String.fromCharCode(((a + b) % 26) + 65); 
+        num = num + 1;
+      }
+      else { 
+        result += message[i];
+      };
+    };
+    if (!this.machine) { 
+      return result.split('').reverse().join('');
+    }
+    else {
+      return result;
+    };
+    };
+  };
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    else {
+      if (key.length < message.length) {
+        for (let i = 0; i < message.length; i++) {
+          key += key[i % key.length];
+        };
+      };
+    key = key.toUpperCase();
+    let mes = message.toUpperCase().match(/[a-z]/gi);
+    if (mes !== null) {
+        mes = mes.join('');
+    };
+    let result = '';
+    let num = 0;
+    for (let i = 0; i < message.length; i++) {     
+      if (message[i].match(/[a-z]/gi) !== null ) {
+        let a = mes.charCodeAt(num) - 65;
+        let b = key.charCodeAt(num) - 65;
+        result += String.fromCharCode(((a + 26 - b) % 26) + 65); 
+        num = num + 1;
+      }
+      else { 
+        result += message[i];
+      };
+    };
+    if (!this.machine) { 
+      return result.split('').reverse().join('');
+    }
+    else {
+      return result;
+    };
+  };
+  };
 }
 
 module.exports = {
